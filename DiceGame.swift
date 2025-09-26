d7b996907d60cab2d78b4a8e6b3a4635a7b8d4e8
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-26 02:50:10 +0000

    Added DiceGame.swift

---
+++ DiceGame.swift
@@ -0,0 +1,52 @@
+//
+//  DiceGame.swift
+//
+//  Created by Atri Sarker
+//  Created on 2025-09-25
+//  Version 1.0
+//  Copyright (c) 2025 Atri Sarker. All rights reserved.
+//
+//  Program that generates a random number,
+//  and then asks the user to guess it.
+//  The program will keep asking until the user gets it right.
+
+// Constant for the minimum roll of the dice.
+let minNum = 1;
+// Constant for the maximum roll of the dice.
+let maxNum = 6;
+
+// Generate a random number between minNum and maxNum [INCLUSIVE]
+let correctNum = Int.random(in: minNum...maxNum)
+// DO WHILE LOOP [REPEAT WHILE IN SWIFT]
+repeat {
+    // RESET COLOR
+    print("\033[0m", terminator="")
+    // Prompt for user's guess
+    print("Enter a guess [\(minNum)-\(maxNum)]: ", terminator="")
+    // Get user's input for the guess
+    let userInput = readLine() ?? ""
+    // Convert the input to an integer
+    if let guess = Int(userInput) {
+        if (guess < minNum || guess > maxNum) {
+            // Error if the user's guess is outside the valid range. [IN RED]
+            print("\033[0;31mERROR: GUESS CAN'T BE OUTSIDE RANGE")
+        } else if (guess > correctNum) {
+            // Message if the user's guess is too high
+            print("\033[0;34mYour guess is too high!")
+        } else if (guess < correctNum) {
+            // Message if the user's guess is too low
+            print("\033[0;34mYour guess is too low!")
+        } else {
+            // If the guess is not higher than the correct number
+            // and if it is not lower than the correct number,
+            // then it must be the correct number
+            // Congratulate the user
+            print("\033[0;32mYou got the number right!")
+            // BREAK OUT OF THE LOOP
+          break;
+        }
+    } else {
+        // Error message for non-numeric guess. [IN RED]
+        print("\033[0;31mERROR: GUESS MUST BE AN INTEGER")
+    }
+} while (true)
